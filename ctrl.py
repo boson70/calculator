c6940c9256d92da2011606409278d10266c9b5da
developer_B <john@email.com>
2023-06-16 10:50:34 +0900

    modify ctrl.py calc

--- ctrl.py
+++ ctrl.py
@@ -3,6 +3,16 @@ class Control:
         self.view = view
         self.connectSignals()
         
+    def calculate(self):
+        pass
+        
+    def connectSignals(self):
+        self.view.btn1.clicked.connect(self.calculate)
+        self.view.btn2.clicked.connect(self.view.clearMessage)class Control:
+    def __init__(self, view):
+        self.view = view
+        self.connectSignals()
+        
     def connectSignals(self):
         self.view.btn1.clicked.connect(self.view.activateMessage)
         self.view.btn2.clicked.connect(self.view.clearMessage)
